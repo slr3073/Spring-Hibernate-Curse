8beeda2a11b3e463cf03aee5276d54216e30cd2c
Romain SALVAN <romain.salvan@g-mail.fr>
2022-02-06 13:57:26 +0100

    Avec une nouvelle implémentation

---
+++ src/main/java/com/slr3073/coach/CoachTir.java
@@ -0,0 +1,8 @@
+package com.slr3073.coach;
+
+public class CoachTir implements Coach{
+    @Override
+    public String getDailyTraining() {
+        return "Tir à la carabine : 3 chargeurs";
+    }
+}
