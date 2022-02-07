a8af1964b1436eb250374781c9e8201af7dfe56d
Romain SALVAN <romain.salvan@g-mail.fr>
2022-02-07 18:30:14 +0100

    init project mvc

---
+++ Project3-SpringMVC/src/main/java/com/slr3073/project3springmvc/Project3SpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.slr3073.project3springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3SpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project3SpringMvcApplication.class, args);
+    }
+
+}
