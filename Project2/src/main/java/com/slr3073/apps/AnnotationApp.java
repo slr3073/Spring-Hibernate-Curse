8f09583baff3320b393e764c62855fd75c7c6683
Romain SALVAN <romain.salvan@g-mail.fr>
2022-02-07 10:30:06 +0100

    Scan des beans avec @Component et id automatique

--- Project2/src/main/java/com/slr3073/apps/AnnotationApp.java
+++ Project2/src/main/java/com/slr3073/apps/AnnotationApp.java
@@ -8,7 +8,7 @@ public class AnnotationApp {
     public static void main(String[] args){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
         //On récupère bien l'entraîneur avec l'annotation @Component
-        Coach coach = context.getBean("myCoachTennis", Coach.class);
+        Coach coach = context.getBean("coachPetanque", Coach.class);
         System.out.println(coach.getDailyWorkout());
         context.close();
     }
--- Project2/src/main/java/com/slr3073/coachs/CoachPetanque.java
+++ Project2/src/main/java/com/slr3073/coachs/CoachPetanque.java
@@ -2,11 +2,10 @@ package com.slr3073.coachs;
 
 import org.springframework.stereotype.Component;
 
-//On aura un bean avec un id automatique "coachTennis"
 @Component
-public class CoachTennis implements Coach {
+public class CoachPetanque implements Coach {
     @Override
     public String getDailyWorkout() {
-        return "Jouer deux match avec Nathan";
+        return "Tu tires ou tu pointes ?";
     }
 }
