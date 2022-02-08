37eff1361a33fa44a7e0a6ccf8d194e65ca1344c
Romain SALVAN <romain.salvan@g-mail.fr>
2022-02-08 11:08:28 +0100

    Passage d'un param url dans la view avec @RequestParam

--- Project3/src/main/java/com/slr3073/controllers/HelloWorldController.java
+++ Project3/src/main/java/com/slr3073/controllers/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.slr3073.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Locale;
@@ -23,6 +24,13 @@ public class HelloWorldController {
     @RequestMapping("/processForm2")
     public String displayNameCapsLock(HttpServletRequest request, Model model){
         String msg = "YO " + request.getParameter("firstName").toUpperCase() + " !!!";
+        model.addAttribute("message", msg);
+        return "hello-world";
+    }
+
+    @RequestMapping("/processForm3")
+    public String displayNameCapsLock2(@RequestParam("firstName") String name, Model model){
+        String msg = "YO " + name.toUpperCase() + " !!!";
         model.addAttribute("message", msg);
         return "hello-world";
     }
