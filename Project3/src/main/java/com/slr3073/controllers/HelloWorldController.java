30a8b52ca665e4e1f5b8b72d14642799077ff5e5
Romain SALVAN <romain.salvan@g-mail.fr>
2022-02-08 10:54:50 +0100

    Passage d'un param url dans la view

--- Project3/src/main/java/com/slr3073/controllers/HelloWorldController.java
+++ Project3/src/main/java/com/slr3073/controllers/HelloWorldController.java
@@ -1,7 +1,11 @@
 package com.slr3073.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
 
 @Controller
 public class HelloWorldController {
@@ -13,6 +17,13 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm(){
+        return "hello-world";
+    }
+
+    @RequestMapping("/processForm2")
+    public String displayNameCapsLock(HttpServletRequest request, Model model){
+        String msg = "YO " + request.getParameter("firstName").toUpperCase() + " !!!";
+        model.addAttribute("message", msg);
         return "hello-world";
     }
 
