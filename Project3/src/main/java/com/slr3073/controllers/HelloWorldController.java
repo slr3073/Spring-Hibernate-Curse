aa47bc58f7e94cea71d199ab6d930ca4b4e70849
Romain SALVAN <romain.salvan@g-mail.fr>
2022-02-08 17:47:36 +0100

    Formulaire et binding avec @ModelAttribute

--- Project3/src/main/java/com/slr3073/controllers/HelloWorldController.java
+++ Project3/src/main/java/com/slr3073/controllers/HelloWorldController.java
@@ -6,33 +6,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Locale;
 
 @Controller
 public class HelloWorldController {
 
     @RequestMapping("/showForm")
     public String showForm(){
-        return "hello-world-form";
+        return "hello-world/hello-world-form";
     }
 
     @RequestMapping("/processForm")
     public String processForm(){
-        return "hello-world";
+        return "hello-world/hello-world";
     }
 
     @RequestMapping("/processForm2")
     public String displayNameCapsLock(HttpServletRequest request, Model model){
         String msg = "YO " + request.getParameter("firstName").toUpperCase() + " !!!";
         model.addAttribute("message", msg);
-        return "hello-world";
+        return "hello-world/hello-world";
     }
 
     @RequestMapping("/processForm3")
     public String displayNameCapsLock2(@RequestParam("firstName") String name, Model model){
         String msg = "YO " + name.toUpperCase() + " !!!";
         model.addAttribute("message", msg);
-        return "hello-world";
+        return "hello-world/hello-world";
     }
 
 }
---
+++ Project3/src/main/java/com/slr3073/controllers/PersonController.java
@@ -0,0 +1,23 @@
+package com.slr3073.controllers;
+
+import com.slr3073.Person;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/person")
+public class PersonController {
+    @RequestMapping("/showForm")
+    public String showForm(Model model){
+        // On rajoute notre attribut qui sera utilisable dans la vue
+        model.addAttribute("person", new Person());
+        return "person/person-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm(@ModelAttribute("person") Person person){
+        return "/person/display-person";
+    }
+}
