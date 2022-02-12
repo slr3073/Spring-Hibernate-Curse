f64bb65f195b0dbb597cbf57963cb08667c09aa5
Romain SALVAN <romain.salvan@g-mail.fr>
2022-02-12 16:47:51 +0100

    Naming hibernate strategies, OneToOne cascade insertion and deletion

--- Project5 - One2One Unidirectional/src/main/java/com/slr3073/Application.java
+++ Project5 - One2One Unidirectional/src/main/java/com/slr3073/Application.java
@@ -1,5 +1,11 @@
 package com.slr3073;
 
+import com.slr3073.entities.Instructor;
+import com.slr3073.entities.InstructorDetail;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +14,28 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        InstructorDetail instructorDetail = new InstructorDetail("https://www.youtube.com/luv2code", "Kayak");
+        Instructor instructor = new Instructor("Romain", "SALVAN","romain.salvan@g-mail.fr", instructorDetail);
+
+        //Insertion en cascade du détail
+        Session session = getCurrentSessionFromConfig();
+        session.beginTransaction();
+        session.save(instructor);
+        session.getTransaction().commit();
+
+
+        //Suppression en cascade du détail
+        session = getCurrentSessionFromConfig();
+        session.beginTransaction();
+        Instructor instructorToDelete = session.get(Instructor.class, instructor.getId());
+        session.delete(instructorToDelete);
+        session.getTransaction().commit();
     }
 
+    public static Session getCurrentSessionFromConfig() {
+        Configuration config = new Configuration();
+        config.configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetail.class);
+        SessionFactory sessionFactory = config.buildSessionFactory();
+        return sessionFactory.getCurrentSession();
+    }
 }
---
+++ Project5 - One2One Unidirectional/src/main/java/com/slr3073/entities/InstructorDetail.java
@@ -0,0 +1,35 @@
+package com.slr3073.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "instructor_details")
+@ToString
+@Setter
+@Getter
+public class InstructorDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(length = 11, name = "id")
+    private long id;
+
+    @Column(length = 100, name = "youtube_channel")
+    private String youtubeChannel;
+
+    @Column(length = 50,  name = "hobby")
+    private String hobby;
+
+    public InstructorDetail() {
+
+    }
+
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+
+}
