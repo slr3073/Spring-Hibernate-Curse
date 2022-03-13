01c5ec1f8734dcf048ef35f336c96e2a31a7581f
Romain SALVAN <romain.salvan@g-mail.fr>
2022-03-13 15:00:07 +0100

    Resolved #1 : création d'une vue et d'un contrôleur basique

---
+++ Project 12 - MVC Hibernate Doremi/src/main/java/doremi/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package doremi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String displayIndex(){
+        return "index";
+    }
+
+}
---
+++ Project 12 - MVC Hibernate Doremi/src/test/java/doremi/controllers/IndexControllerTest.java
@@ -0,0 +1,34 @@
+package doremi.controllers;
+
+import doremi.controllers.IndexController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class IndexControllerTest {
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new IndexController()).build();
+    }
+
+    @Test
+    public void testIndex() throws Exception{
+        // given: un objet MockMvc qui simulate des échanges MVC
+        // when : on simule du requête HTTP de type GET vers "/"
+        // then : la requête est acceptée (status OK) et la vue "index" est rendue
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andDo(print());
+    }
+
+}
